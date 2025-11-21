a2709315dd92e776b7e606b3f5bcc9d19c1e24d0
raflyaulya <auliarafli819@gmail.com>
2025-11-22 00:17:13 +0700

    update new

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bot import bot as teleChatbot
 import telebot
 import bot.handlers  # register all handlers: IMPORTANT!!
 
+# main function 
 def main(): 
     print('bot is running right now...') 
     teleChatbot.infinity_polling()
